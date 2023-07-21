bbb2a6f083c341db83e8589edcfb1e9b89e9e2ae
chinhcd <7a3dqh@gmail.com>
2023-07-21 14:51:07 +0700

    fix

--- src/pages/DichVuMotCuaV2/QuanLyDon/index.tsx
+++ src/pages/DichVuMotCuaV2/QuanLyDon/index.tsx
@@ -12,7 +12,6 @@ const QuanLyDon = (props: { type?: string }) => {
     setTrangThaiQuanLyDon,
     setTrangThaiQuanLyDonThaoTac,
     getAllBieuMauChuyenVienDieuPhoiModel,
-    getAllBieuMauChuyenVienTiepNhanModel,
     setRecord,
     setDanhSach,
     setCondition,
@@ -21,21 +20,11 @@ const QuanLyDon = (props: { type?: string }) => {
     setPage,
     setTypeTraKetQua,
   } = useModel('dvmc.dichvumotcuav2');
-  const { pathname } = window.location;
-  // const { chuyenVienDieuPhoiGetTongSoDonDVMCModel, chuyenVienXuLyGetTongSoDonDVMCModel, idDichVu } =
-  //   useModel('dashboard');
 
   useEffect(() => {
     setLoaiDichVu('DVMC');
-    if (pathname?.includes('chuyenvientiepnhan')) {
-      // getAllBieuMauChuyenVienTiepNhanModel('DVMC');
-    } else {
-      if (pathname?.includes('quanlydondieuphoi')) {
-        // getAllBieuMauChuyenVienDieuPhoiModel('DVMC');
-      } else {
-        getAllBieuMauChuyenVienDieuPhoiModel('DVMC');
-      }
-    }
+
+    getAllBieuMauChuyenVienDieuPhoiModel('DVMC');
 
     return () => {
       setDanhSach([]);
@@ -44,19 +33,6 @@ const QuanLyDon = (props: { type?: string }) => {
       setFilterInfo({});
     };
   }, []);
-
-  // useEffect(() => {
-  //   if (pathname?.includes('quanlydondieuphoi')) {
-  //     if (isDVMC)
-  //       chuyenVienDieuPhoiGetTongSoDonDVMCModel(isDonCanXuLy, isDVMC ? 'DVMC' : 'VAN_PHONG_SO');
-  //     else getAllBieuMauVPSModel();
-  //   } else {
-  //     if (isDVMC)
-  //       chuyenVienXuLyGetTongSoDonDVMCModel(isDonCanXuLy, isDVMC ? 'DVMC' : 'VAN_PHONG_SO');
-  //     else getAllBieuMauVPSModel();
-  //   }
-  // }, [idDichVu]);
-
   return (
     <Card title="Quản lý đơn">
       <Tabs
@@ -67,25 +43,20 @@ const QuanLyDon = (props: { type?: string }) => {
           } else {
             setTypeTraKetQua('');
           }
-          // if (pathname?.includes('quanlydondieuphoi')) {
-          //   chuyenVienDieuPhoiGetTongSoDonDVMCModel(isDonCanXuLy);
-          // } else {
-          //   chuyenVienXuLyGetTongSoDonDVMCModel(isDonCanXuLy);
-          // }
-
-          if (props.type==='Thao tác') {
-            setTrangThaiQuanLyDonThaoTac(key)
-          }else {
-            setTrangThaiQuanLyDon(key);}
+          if (props.type === 'Thao tác') {
+            setTrangThaiQuanLyDonThaoTac(key);
+          } else {
+            setTrangThaiQuanLyDon(key);
+          }
         }}
-        activeKey={props?.type==='Thao tác'?trangThaiQuanLyDonThaoTac:trangThaiQuanLyDon}
+        activeKey={props?.type === 'Thao tác' ? trangThaiQuanLyDonThaoTac : trangThaiQuanLyDon}
       >
         <TabPane
           // tab={`Chờ xử lý (${
           //   recordTongSoDon?.find((item) => item.trangThai === 'PROCESSING')?.soLuong ?? 0
           // })`}
           tab="Chờ xử lý"
-          key={`${props?.type==='Thao tác'?'PENDING':'PROCESSING'}`}
+          key={`${props?.type === 'Thao tác' ? 'PENDING' : 'PROCESSING'}`}
         />
         <TabPane
           // tab={`Duyệt (${recordTongSoDon?.find((item) => item.trangThai === 'OK')?.soLuong ?? 0})`}
@@ -103,7 +74,7 @@ const QuanLyDon = (props: { type?: string }) => {
         <TabPane tab="Đã trả kết quả" key="DA_TRA_KQ" />
       </Tabs>
 
-      <TableQuanLyDon type={props.type}/>
+      <TableQuanLyDon type={props.type} />
     </Card>
   );
 };
