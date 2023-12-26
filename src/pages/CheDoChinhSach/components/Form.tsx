b451c369234948e299a5498d7f0ffaf493cc175c
chinhcd <7a3dqh@gmail.com>
2023-12-26 11:36:36 +0700

    fix

--- src/pages/CheDoChinhSach/components/Form.tsx
+++ src/pages/CheDoChinhSach/components/Form.tsx
@@ -165,17 +165,6 @@ const FormCheDoChinhSach = () => {
 							</Form.Item>
 						</Col>
 					)}
-					<Col xs={24} md={24}>
-						<Form.Item name='trangThai' label='Trạng thái'>
-							<Select
-								placeholder={'Chọn trạng thái'}
-								options={[
-									{ value: 'ok', label: 'Đã nhận' },
-									{ value: 'cancel', label: 'Chưa nhận' },
-								]}
-							/>
-						</Form.Item>
-					</Col>
 				</Row>
 
 				<div className='form-footer'>
