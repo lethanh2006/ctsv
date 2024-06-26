9239ab7aa7787b9ece92d3f4710616bcfe98005a
Phan Thành <quangthanh.phan@aisoft.com.vn>
2024-06-27 00:14:21 +0700

    fix bug

--- src/pages/DaoTaoV2/SinhVien/ThongKe/DanToc.tsx
+++ src/pages/DaoTaoV2/SinhVien/ThongKe/DanToc.tsx
@@ -1,10 +1,12 @@
+import DonutChart from '@/components/Chart/DonutChart';
 import TableStaticData from '@/components/Table/TableStaticData';
 import type { IColumn } from '@/components/Table/typing';
 import { getThongKeDanTocSinhVien } from '@/services/DaoTaoV2/SinhVien';
+import { Col, Row } from 'antd';
 import { useEffect, useState } from 'react';
 import { useModel } from 'umi';
 
-const ThongKeDanToc = () => {
+const ThongKeDanToc = (props: { mode: 'table' | 'donut' }) => {
 	const { record: recHocKy } = useModel('daotaov2.hocky.hocky');
 
 	const [data, setData] = useState<{ danToc: string; tongSoSv: number; gioiTinh: number }[]>([]);
@@ -44,7 +46,32 @@ const ThongKeDanToc = () => {
 		},
 	];
 
-	return <TableStaticData addStt columns={columns} data={data} />;
+	return props.mode === 'table' ? (
+		<TableStaticData addStt columns={columns} data={data} />
+	) : (
+		<Row>
+			<Col span={12}>
+				<DonutChart
+					showTotal
+					formatY={(val) => `${val} sinh viên`}
+					height={320}
+					yLabel={['Sinh viên']}
+					xAxis={data.map((item) => (item.danToc ? item.danToc : 'Không có thông tin'))}
+					yAxis={[data.map((item) => item.tongSoSv)]}
+				/>
+			</Col>
+			<Col span={12}>
+				<DonutChart
+					showTotal
+					formatY={(val) => `${val} sinh viên nữ`}
+					height={320}
+					yLabel={['Sinh viên nữ']}
+					xAxis={data.map((item) => (item.danToc ? item.danToc : 'Không có thông tin'))}
+					yAxis={[data.map((item) => item.gioiTinh)]}
+				/>
+			</Col>
+		</Row>
+	);
 };
 
 export default ThongKeDanToc;
--- src/pages/DaoTaoV2/SinhVien/ThongKe/Nganh.tsx
+++ src/pages/DaoTaoV2/SinhVien/ThongKe/Nganh.tsx
@@ -1,10 +1,12 @@
+import DonutChart from '@/components/Chart/DonutChart';
 import TableStaticData from '@/components/Table/TableStaticData';
 import type { IColumn } from '@/components/Table/typing';
 import { getThongKeNganhSinhVien } from '@/services/DaoTaoV2/SinhVien';
+import { Row, Col } from 'antd';
 import { useEffect, useState } from 'react';
 import { useModel } from 'umi';
 
-const ThongKeNganh = () => {
+const ThongKeNganh = (props: { mode: 'table' | 'donut' }) => {
 	const { record: recHocKy } = useModel('daotaov2.hocky.hocky');
 
 	const [data, setData] = useState<{ nganh: string; tongSoSv: number; gioiTinh: number }[]>([]);
@@ -44,7 +46,32 @@ const ThongKeNganh = () => {
 		},
 	];
 
-	return <TableStaticData addStt columns={columns} data={data} />;
+	return props.mode === 'table' ? (
+		<TableStaticData addStt columns={columns} data={data} />
+	) : (
+		<Row>
+			<Col span={12}>
+				<DonutChart
+					showTotal
+					formatY={(val) => `${val} sinh viên`}
+					height={320}
+					yLabel={['Sinh viên']}
+					xAxis={data.map((item) => (item.nganh ? item.nganh : 'Không có thông tin'))}
+					yAxis={[data.map((item) => item.tongSoSv)]}
+				/>
+			</Col>
+			<Col span={12}>
+				<DonutChart
+					showTotal
+					formatY={(val) => `${val} sinh viên nữ`}
+					height={320}
+					yLabel={['Sinh viên nữ']}
+					xAxis={data.map((item) => (item.nganh ? item.nganh : 'Không có thông tin'))}
+					yAxis={[data.map((item) => item.gioiTinh)]}
+				/>
+			</Col>
+		</Row>
+	);
 };
 
 export default ThongKeNganh;
