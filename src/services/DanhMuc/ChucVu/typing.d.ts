4bf4495db75aac24813a18cfaa890bfa608e27e7
Truong Tuan Anh <truongtuananhkk@gmail.com>
2025-10-08 17:22:47 +0700

    Update

--- src/services/DanhMuc/ChucVu/typing.d.ts
+++ src/services/DanhMuc/ChucVu/typing.d.ts
@@ -1,11 +1,9 @@
 declare module ChucVu {
-	export interface IRecord {
-		_id: string;
-		ma: string;
-		ten: string;
-		createdAt?: string;
-		updatedAt?: string;
-
-		dataPartitionCode: string;
-	}
+  export interface IRecord {
+    _id: string;
+    ma: string;
+    ten: string;
+    createdAt?: string;
+    updatedAt?: string;
+  }
 }
