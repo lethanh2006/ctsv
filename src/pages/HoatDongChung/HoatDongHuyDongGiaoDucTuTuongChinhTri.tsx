5d4aceed202bef7434adbd34245704da4c1c2270
Truong Tuan Anh <truongtuananhkk@gmail.com>
2026-01-09 08:59:33 +0700

    Song ngữ: Giáo dục chính trị tư tưởng

--- src/pages/HoatDongChung/HoatDongHuyDongGiaoDucTuTuongChinhTri.tsx
+++ src/pages/HoatDongChung/HoatDongHuyDongGiaoDucTuTuongChinhTri.tsx
@@ -1,12 +1,15 @@
 import { EHoatDongChungType1, EHoatDongChungType2 } from '@/services/HoatDongChung/constants';
+import { useIntl } from 'umi';
 import HoatDongChung from '.';
 
 const HoatDongHuyDongGiaoDucTuTuongChinhTri = () => {
+	const intl = useIntl();
+
 	return (
 		<HoatDongChung
 			phanLoaiCap1={EHoatDongChungType1.GIAO_DUC_CHINH_TRI_TU_TUONG}
 			phanLoaiCap2={EHoatDongChungType2.HOAT_DONG_HUY_DONG_GIAO_DUC_TU_TUONG_CHINH_TRI}
-			title='Hoạt động huy động giáo dục tư tưởng chính trị'
+			title={intl.formatMessage({ id: 'tutuongchinhtri.title' })}
 		/>
 	);
 };
