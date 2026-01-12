a800bbc01b7c47f81479386ad14e075fd91a90de
tung.dt <tung.dt@melisoft.vn>
2026-01-12 11:29:15 +0700

    update locale and rule.ts

--- src/pages/TienIch/GioiThieu/index.tsx
+++ src/pages/TienIch/GioiThieu/index.tsx
@@ -1,13 +1,17 @@
 import { unitName } from '@/services/base/constant';
 import { Card } from 'antd';
 import '../../TrangChu/components/style.less';
+import { useIntl } from '@umijs/max';
 
 const AboutPage = () => {
+	const intl = useIntl();
 	return (
 		<Card styles={{ body: { height: '100%' } }}>
 			<div className='home-welcome'>
-				<h1 className='title'>GIỚI THIỆU {unitName.toUpperCase()}</h1>
-				<h2 className='sub-title'>HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {unitName.toUpperCase()}</h2>
+				<h1 className='title'>GIỚI THIỆU {(intl.formatMessage({ id: unitName }) || unitName).toUpperCase()}</h1>
+				<h2 className='sub-title'>
+					HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {(intl.formatMessage({ id: unitName }) || unitName).toUpperCase()}
+				</h2>
 			</div>
 		</Card>
 	);
--- src/pages/TrangChu/index.tsx
+++ src/pages/TrangChu/index.tsx
@@ -1,13 +1,17 @@
 import { unitName } from '@/services/base/constant';
 import { Card } from 'antd';
 import './components/style.less';
+import { useIntl } from '@umijs/max';
 
 const TrangChu = () => {
+	const intl = useIntl();
 	return (
 		<Card styles={{ body: { height: '100%' } }} variant='borderless'>
 			<div className='home-welcome'>
 				<h1 className='title'>PHÂN HỆ QUẢN LÝ ĐÀO TẠO</h1>
-				<h2 className='sub-title'>HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {unitName.toUpperCase()}</h2>
+				<h2 className='sub-title'>
+					HỆ THỐNG PHẦN MỀM CHỈ ĐẠO, ĐIỀU HÀNH - {(intl.formatMessage({ id: unitName })).toUpperCase()}
+				</h2>
 			</div>
 		</Card>
 	);
